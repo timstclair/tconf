3a0e3b80f374def17348c40c70da8853dd8ade6c
Tim St. Clair <timsstclair@gmail.com>
2014-09-30 21:11:04 -0700

    majormodes.el

--- emacs/major-modes.el
+++ emacs/major-modes.el
@@ -1,1 +1,30 @@
-;; Placeholder. Delete me.
+;;
+;; Customizations for major modes
+;;
+
+;;
+;; asm-mode
+;;
+
+;; Override indentation of assembler commands to have 0 indentation
+(defun asm-calculate-indentation-override ()
+  (or
+   ;; Flush specific assembler commands to the left margin.
+   (and (looking-at "\\.globl") 0)
+   (and (looking-at "\\.section") 0)
+   (asm-calculate-indentation-original)))
+(eval-after-load "asm-mode"
+  '(progn
+     (fset 'asm-calculate-indentation-original (symbol-function 'asm-calculate-indentation))
+     (fset 'asm-calculate-indentation 'asm-calculate-indentation-override)))
+
+;; Set asm-mode comment character to the @ (for ARM)
+(setq-default asm-comment-char ?@)
+
+;; Cleanup whitespace on save in asm-mode
+(add-hook 'asm-mode-hook
+          (lambda ()
+            (add-hook 'before-save-hook
+                      'whitespace-cleanup
+                      nil
+                      1)))
