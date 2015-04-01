9d8342e853636c45983eb375fa51eacdca2a6faf
Tim St. Clair <timsstclair@gmail.com>
2015-03-31 22:58:35 -0700

    Partial migration to use of new homemaker script

--- lib/homemaker.sh
+++ lib/homemaker.sh
@@ -16,18 +16,29 @@
 
 # hmgl .Xresources ! Xresources/*
 
-
 # Default values for script parameters.
-DEVICE=desktop
+DEVICE= # No default, must be set.
 INPUT=$HOME/tconf
 OUTPUT=$HOME
 GEN_OUT=$INPUT/.gen_files
-
+HM_ERROR_MODE=WARNING
 
 hm_init() {
+  if [ $INPUT/local ]; then
+    return
+  fi
+
+  if [ -z $DEVICE ]; then
+    hm_error "\$DEVICE not set!"
+    return 1
+  fi
+
   hm_link $INPUT/devices/$DEVICE $INPUT/local
 }
 
+# Shortcuts
+hml() { hm_link $@; }
+hmgl() { hm_generate_link $@; }
 
 # Check whether destination exists (and is not already the correct link)
 # Create link from -> to
@@ -35,22 +46,24 @@ hm_link() {
   local SRC=$1
   local DST=$2
 
-  # Make DST absolute.
-  if [ "${DST:0:1}" != "/" ]; then
-    DST=$INPUT/$DST
-  fi
-
   # Make SRC absolute.
   if [ "${SRC:0:1}" != "/" ]; then
-    SRC=$OUTPUT/$SRC
+    SRC=$INPUT/$SRC
+  fi
+
+  # Make DST absolute.
+  if [ "${DST:0:1}" != "/" ]; then
+    DST=$OUTPUT/$DST
   fi
 
   if [ -e "$DST" ]; then
     if [ ! -h "$DST" ]; then
-      return hm_error "$DST exists!"
+      hm_error "$DST exists!"
+      return 1
     fi
     if [ $(readlink "$DST") != "$SRC" ]; then
-      return hm_error "$DST points to \"$(readlink "$DST")\"; expected \"$SRC\""
+      hm_error "$DST points to \"$(readlink "$DST")\"; expected \"$SRC\""
+      return 1
     fi
     # Symlink is already set up.
     return 0
@@ -59,29 +72,27 @@ hm_link() {
   ln -s "$SRC" "$DST"
 }
 
-
 # Concatenate srcs into a file in $GEN_OUT
 # Check source modification times to determine whether it's necessary.
 hm_generate_link() {
   local DST=$1
   local COMMENT=$2
-  local SRCS=
-  SRCS${@:3}
+  local SRCS=${@:3}
 
   if [ ! -d $GEN_OUT ]; then
     if [ -e $GEN_OUT ]; then
       return hm_error"Invalid generated output directory: $GEN_OUT"
     fi
 
-    makedir $GEN_OUT
+    mkdir $GEN_OUT
   fi
 
   local REL_DST=${DST//$HOME\/}   # Make relative to home if necessary.
   local GEN_DST="$GEN_OUT/${DST//\//.}"
 
-  if ! has_changes $GEN_DST; then
+  if ! hm_has_changes $GEN_DST; then
     # No changes, nothing to do.
-    return hm_link $GEN_DST $DST
+    hm_link $GEN_DST $DST || return 1
   fi
 
   hm_generate $GEN_DST $COMMENT $SRCS
@@ -115,8 +126,7 @@ hm_generate() {
 # Prints a file header using COMMENT and listing SRCS
 hm_file_header() {
   local COMMENT=$1
-  local SRCS
-  SRCS=${@:2}
+  local SRCS=${@:2}
 
   if [[ "$COMMENT" == "" ]]; then
     return;
@@ -147,8 +157,11 @@ hm_sect_header() {
 # Check whether any of the SRCS have been modified since DST was created.
 hm_has_changes() {
   local DST=$1
-  local SRCS
-  SRCS=${@:2}
+  local SRCS=${@:2}
+
+  if [ ! -f $DST ]; then
+    return 0
+  fi
 
   local CREATE_TIME=$(date +%s -r $DST)
   for SRC in $SRCS; do
@@ -166,25 +179,14 @@ hm_error() {
   local MSG=$1
 
   case $HM_ERROR_MODE in
-    SILENT)
-      # Do nothing
-      ;;
+    # SILENT) # Do nothing
     WARNING)
       >&2 echo $MSG
+      return 1
       ;;
     FAIL)
       >&2 echo $MSG
       exit 1
       ;;
   esac
-}
-
-# Unset functions & bindings used during homemaker setup.
-hm_unset() {
-  # e.g.
-  unset -f hm_fail
-  unset $HM_ERROR_MODE
-  # etc.
-
-  # TODO
 }
