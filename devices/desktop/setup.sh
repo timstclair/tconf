368c0db19fc3e5d121c3feb3a16431c7f43bcbd5
Tim St. Clair <timsstclair@gmail.com>
2017-04-03 22:55:48 -0700

    Fix gpg ssh config per Arch instructions

--- devices/desktop/setup.sh
+++ devices/desktop/setup.sh
@@ -5,3 +5,5 @@ INPUT=$TCONF/local
 hml shell/bash_profile .bash_profile
 hml etc/xinitrc .xinitrc
 hml etc/yaourtrc .yaourtrc
+hml etc/gpg-agent.conf .gnupg/gpg-agent.conf
+hml etc/pam_environment .pam_environment
